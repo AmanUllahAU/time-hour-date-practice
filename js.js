14296b5ee641f56df0a5c374ef76411d3728f404
Aman Ullah Khan <amanullahkhan81828@gmail.com>
2023-06-12 14:54:25 +0500

    commit

---
+++ js.js
@@ -0,0 +1,23 @@
+
+var dob = new Date();
+console.log(dob);
+
+var days = ['Sun', "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var dayNum = dob.getDay();
+console.log(days[dayNum]);
+
+console.log(dob.getDate());
+
+console.log(dob.getHours());
+
+console.log(dob.getMinutes());
+
+console.log(dob.getSeconds());
+
+
+
+console.log(dob.getMilliseconds());
+
+
+console.log(dob.getFullYear());
+
